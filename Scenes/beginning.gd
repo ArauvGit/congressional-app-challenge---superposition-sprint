ccb53544377d70e5956cb3b7762ac5ca7659eb64
iaminyourattic <meisfrogg@gmail.com>
2026-07-13 17:39:49 -0400

    script
    started adding button scriptr

---
+++ Scenes/beginning.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+
+	
+
+
+func _on_yellow_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_green_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_red_pressed() -> void:
+	pass # Replace with function body.
--- Scenes/button.gd
+++ Scenes/button.gd
@@ -1,11 +1,19 @@
 extends Button
-
-
-# Called when the node enters the scene tree for the first time.
+var button_names: Array = ["yellow", "green", "red", "blue"]
 func _ready() -> void:
 	pass # Replace with function body.
+# Called when the node enters the scene tree for the first time.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _pressed() -> void:
+	for group in get_groups():
+		for group_name in button_names:
+			if group.contains(group_name):
+				pass #add the player set script later
+	
+func _script_tranformation():
+	self.set_script()
