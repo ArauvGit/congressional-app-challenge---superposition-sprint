66ace20efce81225d296014433dfd5e80c5dfc32
iaminyourattic <meisfrogg@gmail.com>
2026-08-29 15:05:36 -0400

    editing sinwave
    now it is animated and better margins

--- Scripts/sinwave.gd
+++ Scripts/sinwave.gd
@@ -1,28 +1,35 @@
-extends Node2D
+extends Line2D
 @export var y: float = 0
-@export var x: float = -320
+@export var x: float = 0
 var position_adder: float = 0
+@onready var players: Node2D = $players
 
 # # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	while x != 321:
-		draw_line(Vector2(x, y), Vector2(x + 1, set_y(20 * sin(0.1 * x))), Color("ffff"), 1.0)
-# 	while x != 321:
-# 		self.add_point(Vector2(x, y))
-# 		print(Vector2(x, y))
-# 		x += 1
-# 		set_y(20 * sin(0.1 * x))
+	while x != 641:
+		self.add_point(Vector2(x, y))
+		print(Vector2(x, y))
+		x += 1
+		set_y(15 * sin(0.15 * x))
+	
 
 
 # # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta: float) -> void:
 # 	pass
-# func _physics_process(delta: float) -> void:
-# 	pass
+func _physics_process(delta: float) -> void:
+	if x >= 641:
+		for i in range(2):
+			self.add_point(Vector2(x, y))
+			x += 1
+			set_y(15 * sin(0.15 * x))
+			self.remove_point(0)
+			self.translate(Vector2(-1, 0))
 func set_y(new_value: float):
 	if y != new_value:
 		y = new_value
 	return y
-#func _draw() -> void:
-	#while x != 321:
-		#draw_line(Vector2(x, y), Vector2(x + 1, set_y(20 * sin(0.1 * x))), Color("ffff"), 1.0)
+func player_position(): 
+	for child in players.get_children(): 
+		if is_instance_valid(child): 
+			child.position = self.get_point_position(11)
