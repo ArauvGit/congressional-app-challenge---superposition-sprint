1e634da4d2cbeb371744343c6c00dcdb222b9b2d
iaminyourattic <meisfrogg@gmail.com>
2026-08-30 13:49:45 -0400

    interference update
    more colors, imported players that now bob up and down , player can move horizontally. first i have to make it so the players animated sprite is offset with a sine thing instead of its position (i need position.y for something else) and i have to make...the actual minigame

--- Scripts/sinwave.gd
+++ Scripts/sinwave.gd
@@ -22,7 +22,7 @@ func _ready() -> void:
 		self.add_point(Vector2(x, y))
 		x += 1
 		set_y(15 * sin(0.15 * x))
-	player_position()
+	set_player_position()
 
 # # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta: float) -> void:
@@ -36,24 +36,50 @@ func _physics_process(delta: float) -> void:
 			set_y(15 * sin(0.15 * x))
 			self.remove_point(0)
 			self.translate(Vector2(-1, 0))
+	update_player_position()
 
 func set_y(new_value: float):
 	if y != new_value:
 		y = new_value
 	return y
 
-func player_position():
+func update_player_position():
+	for child in players.get_children():
+		var horizontal_direction: float = Input.get_axis("move_left", "move_right")
+		var vertical_direction: float = Input.get_axis("move_up", "move_down")
+		if child.is_in_group("player"):
+			child.position.x += 1 * horizontal_direction
+		match child:
+			var player_node when player_node.is_in_group("Yellow"):
+				child.position.y = 15 * sin(0.15 * abs(x) * 1.1)
+			var player_node when player_node.is_in_group("Green"):
+				child.position.y = 15 * sin(0.15 * abs(x) * 1.25) + 60
+			var player_node when player_node.is_in_group("Red"):
+				child.position.y = 15 * sin(0.15 * abs(position.x) * 1.5) + 140
+				#child.position.y += 1 * vertical_direction
+			var player_node when player_node.is_in_group("Blue"):
+				child.position.y = 15 * sin(0.15 * abs(x)) + 220
+
+
+func set_player_position():
 	if is_instance_valid(players):
 		for child in players.get_children():
-			if is_instance_valid(child) and child is CharacterBody2D:
-				match child:
-					var character when character.name == "yellow":
-						%Line1.get_child(0).position = %Line1.get_point_position(11)
-					var character when character.name == "green":
-						%Line2.get_child(0).position = %Line2.get_point_position(11)
-					var character when character.name == "red":
-						%Line3.get_child(0).position = %Line3.get_point_position(11)
-					var character when character.name == "blue":
-						%Line4.get_child(0).global_position = %Line4.get_point_position(11)
+			match child:
+				var character when character.is_in_group("Yellow"):
+					child.global_position = Vector2(50, %Line1.position.y)
+				var character when character.is_in_group("Green"):
+					child.global_position = Vector2(50, %Line2.position.y)
+				var character when character.is_in_group("Red"):
+					child.global_position = Vector2(50, %Line3.position.y)
+				var character when character.is_in_group("Blue"):
+					child.global_position = Vector2(50, %Line4.position.y)
+		#if not is_instance_valid($yellow):
+			#%Line1.queue_free()
+		#if not is_instance_valid($green):
+			#%Line2.queue_free()
+		#if not is_instance_valid($red):
+			#%Line3.queue_free()
+		#if not is_instance_valid($blue):
+			#%Line4.queue_free()
 	else:
 		push_error("players is null!")
